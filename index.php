baffc40ea9d110f92d90d7c4cb76d79459a60417
Alexandra Banica <banica9221@hotmail.be>
2021-02-23 16:06:23 +0100

    include added to autoloader + html

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
 
-spl_autoload_register('myAutoloader');
+include 'includes/autoloader.inc.php';
+?>
 
-function myAutoloader($classname) {
-    $path = "classes/";
-    $extension = ".php";
-    $fullPath = $path . $classname . $extension;
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>blackjack game</title>
+</head>
+<body>
 
-    include_once $fullPath;
-}
-
+</body>
+</html>
