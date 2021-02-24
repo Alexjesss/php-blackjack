b5b4bc6c46d355436e061d7770e243bfbca731df
Alexandra Banica <banica9221@hotmail.be>
2021-02-24 09:44:15 +0100

    changed player to dealer object

--- classes/Blackjack.php
+++ classes/Blackjack.php
@@ -5,18 +5,17 @@ class Blackjack
 {
         private Player $player;
         private Player $dealer;
-        private Deck $Deck;
+        private Deck $deck;
 
 
     public function __construct()
     {
-        $this->player = new Player($this->getDeck());
-        $this->dealer = new Player($this->getDeck());
         $this->deck = new Deck();
         $this->deck->shuffle();
+        $this->player = new Player($this->getDeck());
+        $this->dealer = new Dealer($this->getDeck());
 
     }
-
 
     public function setPlayer($player): void
     {
@@ -29,30 +28,24 @@ class Blackjack
         $this->dealer = $dealer;
     }
 
-
     public function setDeck($Deck): void
     {
-        $this->Deck = $Deck;
+        $this->deck = $Deck;
     }
 
-
-
-    public function getPlayer()
+    public function getPlayer() : Player
     {
         return $this->player;
     }
 
-    public function getDealer()
+    public function getDealer() : Player
     {
         return $this->dealer;
     }
 
-    public function getDeck()
+    public function getDeck(): Deck
     {
-        return $this->Deck;
+        return $this->deck;
     }
-
-
-
-
 }
+
